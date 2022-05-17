cfbec4cd124ca5bf7e0a91d773e7e5115d42a847
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-17 21:51:58 +0300

    #1

---
+++ Task_14_Name_In_Frame/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14_Name_In_Frame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Введите необходимый символ для рамки: ");
+            string symbolForFrame = Console.ReadLine();
+
+            int leghtName = name.Length;
+            string upDownFrames = "";
+            for (int i = -2; i < leghtName; i++)
+                upDownFrames += symbolForFrame;
+
+            Console.WriteLine(upDownFrames);
+            Console.WriteLine(symbolForFrame + name + symbolForFrame);
+            Console.WriteLine(upDownFrames);
+        }
+    }
+}
